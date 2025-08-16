6115451b8aadfcedfaa49d2ed44a90f465e24bbe
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-08-16 21:47:17 +0300

    Refactored, also close yet far from the answer

--- wc.c
+++ wc.c
@@ -2,38 +2,37 @@
 #include <string.h>
 #include <ctype.h>
 
-int main(int argc, char *argv[]) {
-    char *command = argv[1];
-    char *filename = argv[2];
-    if (command != NULL && filename != NULL) {
-        FILE *fptr = fopen(filename, "r");
-        if (fptr == NULL) {
-            printf("File was not opened successfully");
-        } else {
-            int ch;
-            int count = 0;
-            while ((ch = fgetc(fptr)) != EOF) {
-                if (strcmp(command, "-c") == 0) {
-                    count ++;
-                } else if (strcmp(command, "-l") == 0) {
-			if (ch == '\n') {
-				count++;
-			}
-		} else if (strcmp(command, "-w") == 0) {
-			int inword = 0;
-			if (!isspace(ch)) {
-				if (!inword) {
-					count++;
-					inword = 1;
-				}
-			} else {
-				inword = 0;
-			}
-		}
-	    }
-            printf("%d\n", count);
-        }
-    }
 
-    return 0;
+int main(int argc, char *argv[]) {
+	char *command = argv[1];
+	char *filename = argv[2];
+
+	if (command == NULL || filename == NULL) return 1;
+
+	FILE *fptr;
+	fptr = fopen(filename, "r");
+
+	if (fptr == NULL) {
+		printf("Failed to open file");
+	}
+
+	int ch;
+	int count = 0;
+	if (strcmp(command, "-c") == 0) {
+		while((ch = fgetc(fptr)) != EOF) count++;
+	} else if (strcmp(command, "-l") == 0) {
+		while((ch = fgetc(fptr)) != EOF) {
+			if (ch == '\n') count++;
+		}
+	} else if (strcmp(command, "-w") == 0) {
+		while((ch = fgetc(fptr)) != EOF) {
+			if (!isalnum(ch) && isspace(ch)) count++;
+		}
+	}
+
+	printf("%d\n", count);
+
+	fclose(fptr);
+
+	return 0;
 }
